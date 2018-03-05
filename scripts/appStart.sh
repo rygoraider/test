f51e3806d14580d3eb79b9c71323a045f6c9e3a4
Ott Lints <ott.lints@tgn.teleplan.com>
2018-03-05 16:38:09 +0200

    test

--- scripts/appStart.sh
+++ scripts/appStart.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/expect
-send "pm2 start test\r"
-expect {
-    "Applying action" { send "exit\r"}
-    "script not found" { exit 1}
-    timeout { exit 1 }
-}
-expect "siteserver"
-send "exit\r"
-expect "#"
-exit 0
-
+#!/bin/bash
+pm2 start test
+done
